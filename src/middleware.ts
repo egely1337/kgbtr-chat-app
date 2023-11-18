4ecfee8e11be2a59b8652173f72d19b78b986030
egely1337 <hyperamabanyiyen@gmail.com>
2023-11-18 22:01:12 +0300

    settings

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse, type NextRequest } from 'next/server'
+import { getToken } from 'next-auth/jwt'
+
+export async function middleware(request: NextRequest) {
+  const session = await getToken({req: request, secret: process.env.SECRET});
+
+  if(!session) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/chat"]
+}
